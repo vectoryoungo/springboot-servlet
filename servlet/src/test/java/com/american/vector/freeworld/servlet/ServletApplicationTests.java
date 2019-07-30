fb8e1916d6491ab342e1ad35c670fe2190e21398
居洪涛 <hongtao.ju@blockchain001.com>
2019-07-30 17:48:43 +0800

    add redis

--- servlet/src/test/java/com/american/vector/freeworld/servlet/ServletApplicationTests.java
+++ servlet/src/test/java/com/american/vector/freeworld/servlet/ServletApplicationTests.java
@@ -1,12 +1,17 @@
 package com.american.vector.freeworld.servlet;
 
+import com.american.vector.freeworld.servlet.pojo.UserJPA;
 import com.american.vector.freeworld.servlet.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes=ServletApplication.class)
@@ -14,7 +19,8 @@ public class ServletApplicationTests {
 
 	@Autowired
 	private UserService userService;
-
+	@Autowired
+	private RedisTemplate<String,Object> redisTemplate;
 	@Test
 	public void contextLoads() {
 	}
@@ -26,6 +32,18 @@ public class ServletApplicationTests {
 		System.out.println("=========================");
 
 		System.out.println(this.userService.findAll());
+	}
+
+	@Test
+	public void setCache() {
+		List<UserJPA> userJPAList = this.userService.findAll();
+		redisTemplate.opsForValue().set("users",userJPAList);
+	}
+
+	@Test
+	public void getCache() {
+		System.out.println("$$$$$$$$$$" + redisTemplate.opsForValue().get("users"));
+
 	}
 
 
