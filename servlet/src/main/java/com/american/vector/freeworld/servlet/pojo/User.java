27804ffa1447e5c17fbf2d783f885f1991895bf4
居洪涛 <hongtao.ju@blockchain001.com>
2019-07-30 14:59:05 +0800

    add hibernate validator

--- servlet/src/main/java/com/american/vector/freeworld/servlet/pojo/User.java
+++ servlet/src/main/java/com/american/vector/freeworld/servlet/pojo/User.java
@@ -1,9 +1,21 @@
 package com.american.vector.freeworld.servlet.pojo;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public class User {
 	
 	private Long id;
+	@NotBlank
+	@Length(max = 12,min = 2,message = "name lenght must between 2 and 12")
 	private String name;
+	@Min(value = 16)
+	@Max(value = 150)
+	@NotNull
 	private int age;
 	@Override
 	public String toString() {
